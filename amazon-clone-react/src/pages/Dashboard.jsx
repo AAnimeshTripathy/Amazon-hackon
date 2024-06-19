657463d542c27168119c1b765f708cb214cec87f
Animesh Tripathy <148396210+AAnimeshTripathy@users.noreply.github.com>
2024-06-19 06:29:30 +0530

    Add files via upload

---
+++ amazon-clone-react/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function Dashboard() {
+  return (
+    <div>
+      <h2>Budget Dashboard</h2>
+      <p>Welcome to the Amazon</p>
+    </div>
+  );
+}
+
+export default Dashboard;
