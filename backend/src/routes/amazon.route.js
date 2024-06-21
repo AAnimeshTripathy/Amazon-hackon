ea46be0f9acf5ff850156856beee974708780d40
AAnimeshTripathy <aanimeshtripathy@gmail.com>
2024-06-22 04:07:57 +0530

    testing

--- backend/src/routes/amazon.route.js
+++ backend/src/routes/amazon.route.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-
+import User from "../models/user.model.js";
 const router = Router();
 
+// testing (Working Database is successfully connected)
 router.route('/hello').get((req, res) => {
     res.send('Hello from Amazon route');
+    const user = new User({email_id: 'a@gmail.com'});
+    user.save()
+        .then(() => console.log('User added'))
+        .catch(err => console.log(err));
 });
 
 export default router;
